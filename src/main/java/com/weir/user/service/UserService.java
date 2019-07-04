48759bbf84731579d1f088e99f9e00ab7e3cec5b
fly_basket <zh_huan88@126.com>
2019-07-04 14:17:49 +0800

    删除mybatis

--- src/main/java/com/weir/user/service/UserService.java
+++ src/main/java/com/weir/user/service/UserService.java
@@ -1,19 +1,18 @@
 package com.weir.user.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.weir.user.entity.User;
-import com.weir.user.mapper.UserMapper;
 
 @Service
 public class UserService {
-	
-	@Autowired
-	private UserMapper userMapper;
-	
-	public User findUserById (Long id){
-		return userMapper.selectById(id);
+
+	// @Autowired
+	// private UserMapper userMapper;
+
+	public User findUserById(Long id) {
+		// return userMapper.selectById(id);
+		return new User();
 	}
-	
+
 }
